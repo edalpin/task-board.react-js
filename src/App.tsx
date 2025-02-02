cfd20e49cfb80ea10553d1e8000efca550959c5f
Edwin Alvarez <niwdesk9@gmail.com>
2025-02-02 17:44:51 -0500

    Initial configuration

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+import { DatePicker } from './components/custom/date-picker';
+import { Button } from './components/ui/button';
+
+function App() {
+  return (
+    <>
+      <div className="bg-sky-50">holi</div>
+      <Button>asdasd</Button>
+      <DatePicker></DatePicker>
+    </>
+  );
+}
+
+export default App;
