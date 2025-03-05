dfc077c6e9e29383208bab89cb89867e574ab4dd
Edwin Alvarez <niwdesk9@gmail.com>
2025-03-04 19:46:26 -0500

    Add axios and shadcn components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import './App.css';
+import { Button } from '@/components/ui/button';
 
 function App() {
-  return <>Hello world</>;
+  return (
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button>Click me</Button>
+    </div>
+  );
 }
 
 export default App;
